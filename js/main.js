3673fe1d5ec3db7992a3b63893528a149105d202
Ktyby <kutskolukes@gmail.com>
2020-04-22 20:32:37 +0300

    Кирилл, проерь, пожалуйста

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@ const MIN_NUMBER_LIKES = 15;
 const MAX_NUMBER_LIKES = 200;
 const MIN_NUMBER_COMMENT = 0;
 const MIN_NUMBER_NAME = 0;
+const MIN_NUMBER_DESCRIPTION = 0;
 
-const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const pictureTemplate = document.querySelector("#picture").content.querySelector(".picture");
+const picturePreview = document.querySelector(".big-picture__preview");
+const bigPicture = document.querySelector(".big-picture");
 
-const commentsData = [
+const COMMENTS_DATA = [
     "Всё отлично!",
     "В целом всё неплохо. Но не всё.",
     "Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально",
@@ -14,17 +17,17 @@ const commentsData = [
     "Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!"
 ];
 
-const usersName = [
+const USERS_NAME = [
     "Максим",
     "Диана",
     "Виолетта",
-    "Захар",
+    "Захарушка",
     "Анна",
     "Пётр",
     "Наталия"
 ];
 
-const photosUrls = [
+const PHOTOS_URLS = [
     "photos/1.jpg",
     "photos/2.jpg",
     "photos/3.jpg",
@@ -52,19 +55,33 @@ const photosUrls = [
     "photos/25.jpg"
 ];
 
-const picturesData = [];
+DESCRIPTION_DATA = [
+    "Если смогу, я сделаю это. Конец истории.",
+    "Я не ленивый. Просто у меня нет мотивации.",
+    "Я — это мы.",
+    "Просто ешьте, живите и любите. Затем с утра повторяйте каждый день.",
+    "Любите меня, от этого я буду сиять еще ярче.",
+    "Утром, только одна хорошая мысль меняет смысл целого дня.",
+    "Никогда не позволяйте никому скучать.",
+    "Я пыталась заниматься йогой, но в позе лотоса уснула.",
+    "Всегда начинайте свой день с хороших людей и кофе.",
+    "Да, я сумасшедшая. Быть нормальной — слишком скучно."
+];
+
+const PICTURES_DATA = [];
 
 const getRandomInteger = (minValue, maxValue) => {
     return Math.floor(Math.random() * (maxValue - minValue)) + minValue;
 }
 
 const generatePicturesData = () => {
-    for (let index = 0; index < photosUrls.length; index++) {
-        picturesData.push({
-            comments: commentsData[getRandomInteger(MIN_NUMBER_COMMENT, commentsData.length - 1)],
+    for (let index = 0; index < PHOTOS_URLS.length; index++) {
+        PICTURES_DATA.push({
+            comments: COMMENTS_DATA[getRandomInteger(MIN_NUMBER_COMMENT, COMMENTS_DATA.length - 1)],
             likes: getRandomInteger(MIN_NUMBER_LIKES, MAX_NUMBER_LIKES),
-            name: usersName[getRandomInteger(MIN_NUMBER_NAME, usersName.length - 1)],
-            avatar: photosUrls[index]
+            name: USERS_NAME[getRandomInteger(MIN_NUMBER_NAME, USERS_NAME.length - 1)],
+            avatar: PHOTOS_URLS[index],
+            description: DESCRIPTION_DATA[getRandomInteger(MIN_NUMBER_DESCRIPTION, DESCRIPTION_DATA.length - 1)]
         })
     }
 }
@@ -72,22 +89,43 @@ const generatePicturesData = () => {
 const createPicture = (picture) => {
     const image = pictureTemplate.cloneNode(true);
 
-    image.querySelector('.picture__comments').textContent = picture.comments.length;
-    image.querySelector('.picture__likes').textContent = picture.likes;
-    image.querySelector('.picture__img').src = picture.avatar;
+    image.querySelector(".picture__comments").textContent = picture.comments.length;
+    image.querySelector(".picture__likes").textContent = picture.likes;
+    image.querySelector(".picture__img").src = picture.avatar;
 
     return image;
 }
 
 const renderAllPictures = () => {
-    const picturesContainer = document.querySelector('.pictures');
+    const picturesContainer = document.querySelector(".pictures");
     const picturesFragment = document.createDocumentFragment();
 
-    picturesData.forEach((picture) => {
+    PICTURES_DATA.forEach((picture) => {
         picturesFragment.append(createPicture(picture));
     });
 
     picturesContainer.append(picturesFragment);
+}
+
+const createFullScreenPicture = (picture) => {
+    const image = picturePreview.cloneNode(true);
+
+    image.querySelector(".big-picture__img").src = picture.avatar;
+    image.querySelector(".likes-count").textContent = picture.likes;
+    image.querySelector(".comments-count").textContent = picture.comments.length;
+    image.querySelector(".social__comments").textContent = picture.comments;
+
+    return image;
+}
+
+const renderFullScreenPicture = () => {
+    const miniaturs = document.querySelector(".picture__img");
+    miniaturs.addEventListener("click", renderFullScreenPicture);
+
+    PICTURES_DATA.forEach((picture) => {
+        miniaturs.append(createFullScreenPicture(picture));
+    });
+    picturePreview.append(picturesFragment);
 }
 
 generatePicturesData();
